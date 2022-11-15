d0e6851c4a1309a67f204d32621d4ca71fd375b6
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2022-11-15 18:46:25 +0530

    done with image-slider

--- image-slider/src/App.js
+++ image-slider/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Slider from './components/slider';
 
 function App() {
+  const slides = [
+    {
+      url:  "https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg", 
+      title: "1"
+    },
+    
+    {
+       url: "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg",
+        title: "2" 
+    },
+    {
+       url: "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072821__340.jpg",
+     title: "3" },
+    { 
+      url: "https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297__340.jpg", 
+    title: "4" },
+    { 
+      url: "https://cdn.pixabay.com/photo/2012/03/01/00/55/flowers-19830__340.jpg", 
+    title: "5" },
+  ];
+  const contStyle = {
+    width: "500px",
+    height: "280px",
+    margin: "0 auto",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* className="sliderStyle" */}
+       <div style={contStyle}>
+        <Slider slides={slides} />
+      </div>
     </div>
   );
 }
